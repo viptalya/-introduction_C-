f8abc55c958509f01899b2be140b035501915c41
vitalii <vip_10@bk.ru>
2021-03-13 20:13:20 +0300

    lesson1

---
+++ lesson1/lesson1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите своё имя");
+
+            var name = Console.ReadLine();
+
+            var date = DateTime.Now;
+
+            Console.WriteLine($"Здравсвуйте {name}, сейчас {date}");
+        }
+    }
+}
