eda31f60cbf3dc55f830b67fa18904cf064b7cb2
vitalii <vip_10@bk.ru>
2021-04-06 16:30:05 +0300

    lesson6

---
+++ lesson6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Написать консольное приложение Task Manager, которое выводит на экран запущенные процессы и
+            позволяет завершить указанный процесс. Предусмотреть возможность завершения процессов с
+            помощью указания его ID или имени процесса. В качестве примера можно использовать консольные
+            утилиты Windows tasklist и taskkill.
+            */
+            try
+            {
+                Process process = Process.Start("notepad.exe");
+                Console.WriteLine(process.Id) ;
+                Console.WriteLine("Введите ID процесса, который нужно завершить");
+                int id = Convert.ToInt32(Console.ReadLine());
+                if(id == process.Id)
+                {
+                    process.Kill();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Что-то пошло не так :)");
+            }
+        }
+    }
+}
